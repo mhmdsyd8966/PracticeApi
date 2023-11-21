89b36a22b1b105a8360161eaaf94eaed0735f3a9
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2023-11-21 10:17:14 +0200

    order controller

---
+++ zzApi/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Iservices;
+using WebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace zzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+
+        }
+
+        
+        // POST api/<OrderController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Cart cart)
+        {
+            var order = _orderService.AddOrder(cart);
+            return Ok(order);
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var order =_orderService.DeleteOrder(id);
+            return Ok(order);
+        }
+    }
+}
